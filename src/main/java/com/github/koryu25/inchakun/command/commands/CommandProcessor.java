7bacf72abecc9d7e94c8c6d4fd2c69af4009cf36
Koryu25 <komu.ryu@icloud.com>
2022-09-28 05:09:14 +0900

    [add]: CommandProcessorの追加

---
+++ src/main/java/com/github/koryu25/inchakun/command/commands/CommandProcessor.java
@@ -0,0 +1,8 @@
+package com.github.koryu25.inchakun.command.commands;
+
+import com.github.koryu25.inchakun.command.ConsoleCommand;
+
+public interface CommandProcessor {
+
+    void run(ConsoleCommand command);
+}
