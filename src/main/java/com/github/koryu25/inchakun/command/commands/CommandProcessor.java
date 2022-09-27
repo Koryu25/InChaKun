6128108e4c5a85acc9fd6ddd20b281550094884e
Koryu25 <komu.ryu@icloud.com>
2022-09-28 05:09:54 +0900

    [add]: Commandの名前変更

--- src/main/java/com/github/koryu25/inchakun/command/commands/CommandProcessor.java
+++ src/main/java/com/github/koryu25/inchakun/command/commands/CommandProcessor.java
@@ -1,8 +1,8 @@
 package com.github.koryu25.inchakun.command.commands;
 
-import com.github.koryu25.inchakun.command.ConsoleCommand;
+import com.github.koryu25.inchakun.command.Command;
 
 public interface CommandProcessor {
 
-    void run(ConsoleCommand command);
+    void run(Command command);
 }
