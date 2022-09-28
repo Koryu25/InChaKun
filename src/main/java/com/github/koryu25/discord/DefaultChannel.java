545095ece6534d46f86eb4cddfbcf850a0e7a43d
Koryu25 <komu.ryu@icloud.com>
2022-09-28 18:49:03 +0900

    [update]: idをStringに変更

--- src/main/java/com/github/koryu25/discord/DefaultChannel.java
+++ src/main/java/com/github/koryu25/discord/DefaultChannel.java
@@ -6,17 +6,17 @@ import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 
 @AllArgsConstructor
 public enum DefaultChannel {
-    HITORIGOTO("独り言", 923174877534486528L),
-    ZATUDAN_ONE("雑談1", 837341373547544596L),
-    ZATUDAN_TWO("雑談2", 837341635146022932L),
-    ZATUDAN_THREE("雑談3", 837341665114980443L),
-    KIKISEN_ONE("大聞き専1", 845980155360837632L),
-    KIKISEN_TWO("大聞き専2", 849254065170284554L);
+    HITORIGOTO("独り言", "923174877534486528"),
+    ZATUDAN_ONE("雑談1", "837341373547544596"),
+    ZATUDAN_TWO("雑談2", "837341635146022932"),
+    ZATUDAN_THREE("雑談3", "837341665114980443"),
+    KIKISEN_ONE("大聞き専1", "845980155360837632"),
+    KIKISEN_TWO("大聞き専2", "849254065170284554");
 
     @Getter
     private String jp;
     @Getter
-    private long id;
+    private String id;
 
     public static DefaultChannel fromJp(String jp) {
         for (DefaultChannel channel : DefaultChannel.values()) {
